9c3ed03232f3c0c8c54e7ee6e80f3401e0cf89d3
Kenichi Hasegawa <kenichi.hasegawa@dena.com>
2019-07-10 19:02:55 +0900

    GOODBYE Oh-my-zsh!

--- .oh-my-zsh/themes/alien/libs/promptlib/modules/background_job.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-plib_bg_count() {
-  __jobc="`jobs | grep -v "pwd" | wc -l | tr -d ' '`";
-  if [[ "$__jobc" != 0 ]]; then
-    echo -ne "[$__jobc] "
-  fi
-  unset __jobc;
-}
--- .oh-my-zsh/themes/alien/modules/svn.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-alien_is_svn(){
-  echo `plib_is_svn`;
-}
-
-alien_svn_branch() {
-  echo -n " ${ALIEN_SVN_SYM}: @`plib_svn_rev` ";
-}
