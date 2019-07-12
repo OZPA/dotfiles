d9d4988f7f22b2e51e5d10365cef161a6b0a8f7f
Kenichi Hasegawa <kenichi.hasegawa@dena.com>
2019-07-12 22:59:28 +0900

    ディレクトリを省略しないように修正

--- config.fish
+++ config.fish
@@ -4,6 +4,9 @@ set -x PATH /usr/local/bin $PATH
 # superior nodebrew
 set -x PATH $HOME/.nodebrew/current/bin $PATH
 
+# prompt directory not abbreviation
+set -g fish_prompt_pwd_dir_length 0
+
 # key bind
 function fish_user_key_bindings
   bind \cr peco_select_history # Bind for peco select history to Ctrl+R
