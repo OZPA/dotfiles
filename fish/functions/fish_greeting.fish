7c992578461b5db0f960c55cbd09eef397322fad
Kenichi Hasegawa <kenichi.hasegawa@dena.com>
2019-08-23 18:17:07 +0900

    fix greeting

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,6 +1,6 @@
 function fish_greeting
   echo "＼今日も一日がんばるぞい！／"
   # curl -s https://cdn.hinatazaka46.com/images/14/3b3/774d0334ec1fc84ab60426408d706.jpg | imgcat
-  set n (random 1 27)
-  imgcat $HOME/.greeting_img/$n.jpg
+  # set n (random 1 27)
+  # imgcat $HOME/.greeting_img/$n.jpg
 end
